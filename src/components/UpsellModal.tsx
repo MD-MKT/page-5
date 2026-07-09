b028115a5dacb06c129fe787271513251218fb8c
MD-MKT <Mdmarketingaus@gmail.com>
2026-07-09 15:59:12 -0300

    Add status and category to upsell modal data
    Added status and category data fields to the payload.

--- src/components/UpsellModal.tsx
+++ src/components/UpsellModal.tsx
@@ -55,6 +55,8 @@ export function UpsellModal({
         first_name: firstName || cleanName,
         last_name: lastNameParts.join(" "),
         program_name: "Intro to Padel - Page 4",
+        "data[status]": "captured",
+        "data[category]": "program",
         "data[user][first_name]": firstName || cleanName,
         "data[user][last_name]": lastNameParts.join(" "),
         "data[user][email]": lead.email,
