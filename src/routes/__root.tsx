6d049519950b023def732263eb61909c865bf851
Derick dos santos <mdmarketingaus@gmail.com>
2026-07-31 11:00:55 -0300

    Use Page 5 Clarity project

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -131,7 +131,7 @@ function RootShell({ children }: { children: React.ReactNode }) {
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "xlwdqju4yg");
+              })(window, document, "clarity", "script", "xv35titui1");
             `,
           }}
         />
